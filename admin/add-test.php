b3526ebc7b34c993ca02964447f0ba7cc8a19ab1
Ashish <ashishmkumar69@gmail.com>
2023-06-16 12:42:22 +0530

    added admin part of managing lab test deatils

---
+++ admin/add-test.php
@@ -0,0 +1,138 @@
+<?php
+session_start();
+error_reporting(0);
+include('include/dbconnection.php');
+if (strlen($_SESSION['odlmsaid']==0)) {
+    header('location:logout.php');
+} else {
+        if(isset($_POST['submit']))
+        {
+            $title=$_POST['title'];
+            $desc=$_POST['discription'];
+            $interpretation=$_POST['interpretation'];
+            $price=$_POST['price'];
+
+            $sql="insert into tbllabtest(TestTitle,TestDescription,Testinterpretation,Price) values (:title,:discription,:interpretation,:price)";
+            $query=$dbh->prepare($sql);
+            $query->bindParam(':title',$title,PDO::PARAM_STR);
+            $query->bindParam(':discription',$discription,PDO::PARAM_STR);
+            $query->bindParam(':interpretation',$interpretation,PDO::PARAM_STR);
+            $query->bindParam(':Price',$price,PDO::PARAM_STR);
+            $query->execute();
+
+            $LastInsertedId=$dbh->lastInsertId();
+            if ($LastInsertedId>0){
+                echo '<script>alert("Test Deatils has been added")</script>';
+                echo "<script>Window.location.href='add-test.php'</script>";    
+            }
+            else {
+                echo '<script>alert("Somenthing went Wrong!! Please try again")</script>';
+            }
+        }
+?>
+
+<!DOCTYPE html>
+<html lan="en">
+    <head>
+        <title>Health Med Labs Test Deatils</title>
+        <link rel="stylesheet" href="libs/bower/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="libs/bower/material-design-iconic-font/dist/css/material-design-iconic-font.css">
+  <!-- build:css assets/css/app.min.css -->
+  <link rel="stylesheet" href="libs/bower/animate.css/animate.min.css">
+  <link rel="stylesheet" href="libs/bower/fullcalendar/dist/fullcalendar.min.css">
+  <link rel="stylesheet" href="libs/bower/perfect-scrollbar/css/perfect-scrollbar.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/core.css">
+  <link rel="stylesheet" href="assets/css/app.css">
+  <!-- endbuild -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700,800,900,300">
+  <script src="libs/bower/breakpoints.js/dist/breakpoints.min.js"></script>
+  <script>
+    Breakpoints();
+  </script>
+  
+</head>
+<body class="menubar-left menubar-unfold menubar-light theme-primary">
+<?php include_once('includes/header.php');?>
+
+<?php include_once('includes/sidebar.php');?>
+
+<main id="app-main" class="app-main">
+<div class="wrap">
+  <section class="app-content">
+    <div class="row">
+
+    <div class="col-md-12">
+        <div class="widget">
+          <header class="widget-header">
+            <h3 class="widget-title">Add Test Detail</h3>
+          </header>
+
+          <hr class="widget-separator">
+          <div class="widget-body">
+           
+            <form class="form-horizontal" method="post">
+              <div class="form-group">
+                <label for="exampleTextInput1" class="col-sm-3 control-label">Test Title:</label>
+                <div class="col-sm-9">
+                  <input type="text" class="form-control" id="exampleTextInput1" name="title" value="" required='true'>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="email2" class="col-sm-3 control-label">Test Description:</label>
+                <div class="col-sm-9">
+                  <textarea type="text" class="form-control" id="email2" name="description" value="" required="true"></textarea>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="email2" class="col-sm-3 control-label">Test Interpretation:</label>
+                <div class="col-sm-9">
+                  <textarea type="text" class="form-control" id="email2" name="interpretation" value="" required='true'></textarea>
+                </div>
+              </div>
+               <div class="form-group">
+                <label for="email2" class="col-sm-3 control-label">Price:</label>
+                <div class="col-sm-9">
+                  <input type="text" class="form-control" id="email2" name="price" value="" required='true'>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-sm-9 col-sm-offset-3">
+                  <button type="submit" class="btn btn-success" name="submit">Add</button>
+                </div>
+              </div>
+            </form>
+    </div>
+    </div>
+    </div>
+    </div>
+    </section>
+    </div>
+    </section>
+    </div>
+    <?php include_once('includes/footer.php');?>
+    </main>
+    <?php include_once('includes/customizer.php');?>
+
+
+  <script src="libs/bower/jquery/dist/jquery.js"></script>
+  <script src="libs/bower/jquery-ui/jquery-ui.min.js"></script>
+  <script src="libs/bower/jQuery-Storage-API/jquery.storageapi.min.js"></script>
+  <script src="libs/bower/bootstrap-sass/assets/javascripts/bootstrap.js"></script>
+  <script src="libs/bower/jquery-slimscroll/jquery.slimscroll.js"></script>
+  <script src="libs/bower/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
+  <script src="libs/bower/PACE/pace.min.js"></script>
+
+  <script src="assets/js/library.js"></script>
+  <script src="assets/js/plugins.js"></script>
+  <script src="assets/js/app.js"></script>
+
+  <script src="libs/bower/moment/moment.js"></script>
+  <script src="libs/bower/fullcalendar/dist/fullcalendar.min.js"></script>
+  <script src="assets/js/fullcalendar.js"></script>
+
+    </body>
+</html>
+<?php }  ?>
+
